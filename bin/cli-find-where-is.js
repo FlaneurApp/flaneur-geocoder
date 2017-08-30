45790365595052003adb29120cafec49ae6dc4d7
Mickaël Floc’hlay <mickael@flochlay.com>
2017-08-30 13:48:51 +0200

    Add lint & doc scripts

--- bin/cli-find-where-is.js
+++ bin/cli-find-where-is.js
@@ -17,11 +17,17 @@ const paris = 'ChIJD7fiBh9u5kcRYJSMaMOCCwQ'
 const boston = 'ChIJGzE9DS1l44kRoOhiASS_fHg'
 const seattle = 'ChIJVTPokywQkFQRmtVEaUZlJRA'
 
+const brestCoord =     { latitude: 48.390394,   longitude: -4.48607599  }
+const barcelonaCoord = { latitude: 41.38506389, longitude: 2.1734035    }
+const parisCoord =     { latitude: 48.856614,   longitude: 2.3522219    }
+const bostonCoord =    { latitude: 42.3600825,  longitude: -71.0588801  }
+const seattleCoord =   { latitude: 47.6062095,  longitude: -122.3320708 }
+
 // Places
 const brestTourDuMonde = 'ChIJo7G3fCC6FkgROZBxeqhK1T0'
 
 const geocoder = new FlaneurGeocoder(process.env.GOOGLE_PLACES_FLANEUR_API_KEY, true)
-geocoder.findWhereIs(brittany)
+geocoder.findWhereIs(barcelonaCoord.latitude, barcelonaCoord.longitude)
 .then((result) => {
   console.log(result)
 })
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+
 
 const FlaneurGeocoder = require('./lib/flaneur-geocoder')
 
